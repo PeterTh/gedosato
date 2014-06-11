2c2acf7ab5c16d9ed1c9ef3007c573b5f73a4b0f
Peter Thoman <peter@metaclassofnil.com>
2014-06-11 18:45:47 +0200

    Potentially fixed Windows Vista compatibility

--- source/version.cpp
+++ source/version.cpp
@@ -6,9 +6,9 @@ const char* VER_NAME = "Seishoujo Sacrifice";
 
 const unsigned VER_MAJOR = 0;
 const unsigned VER_MINOR = 7;
-const unsigned VER_BUILD = 652;
+const unsigned VER_BUILD = 654;
 
-const char* VER_STRING = "version 0.7.652 built on 2014/06/10 18:46:35";
+const char* VER_STRING = "version 0.7.654 built on 2014/06/11 18:44:37";
 
 #ifdef _DEBUG
 const char* MODE_STRING = "DEBUG";
