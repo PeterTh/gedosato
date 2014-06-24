6d26232ff21e11d917c0eccaad3e515ea36a173a
Peter Thoman <peter@metaclassofnil.com>
2014-06-25 01:33:33 +0200

    Asynchronous screenshots

--- include/imaging.h
+++ include/imaging.h
@@ -14,9 +14,10 @@ class ImageWriter {
 
 public:
 	ImageWriter(IDirect3DDevice9* dev, int wmax, int hmax) : device9(dev), tempSurf(NULL) {
-		device9->CreateRenderTarget(wmax, hmax, D3DFMT_A8B8G8R8, D3DMULTISAMPLE_NONE, 0, TRUE, &tempSurf, NULL);
+		device9->CreateRenderTarget(wmax, hmax, D3DFMT_A8R8G8B8, D3DMULTISAMPLE_NONE, 0, TRUE, &tempSurf, NULL);
 	}
 	~ImageWriter() {
+		if(future.valid()) future.get();
 		SAFERELEASE(tempSurf);
 	}
 
--- source/version.cpp
+++ source/version.cpp
@@ -6,9 +6,9 @@ const char* VER_NAME = "Rapidity is a justice";
 
 const unsigned VER_MAJOR = 0;
 const unsigned VER_MINOR = 8;
-const unsigned VER_BUILD = 777;
+const unsigned VER_BUILD = 796;
 
-const char* VER_STRING = "version 0.8.777 built on 2014/06/24 19:21:40";
+const char* VER_STRING = "version 0.8.796 built on 2014/06/25 01:31:20";
 
 #ifdef _DEBUG
 const char* MODE_STRING = "DEBUG";
