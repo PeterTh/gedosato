a357c57bc659ff1dfe6fb061583cf33c65d5a50b
Peter Thoman <peter@metaclassofnil.com>
2014-06-25 21:05:55 +0200

    Made screenshots *more* asynchronous

--- include/imaging.h
+++ include/imaging.h
@@ -10,15 +10,15 @@
 class ImageWriter {
 	IDirect3DDevice9* device9;
 	IDirect3DSurface9* tempSurf;
-	std::future<void> future;
+	std::vector<std::future<void>> futures;
 
 public:
 	ImageWriter(IDirect3DDevice9* dev, int wmax, int hmax) : device9(dev), tempSurf(NULL) {
 		device9->CreateRenderTarget(wmax, hmax, D3DFMT_A8R8G8B8, D3DMULTISAMPLE_NONE, 0, TRUE, &tempSurf, NULL);
 	}
 	~ImageWriter() {
-		if(future.valid()) future.get();
 		SAFERELEASE(tempSurf);
+		// "futures" gets destroyed, since they futures are created by async they will wait
 	}
 
 	void writeSurface(const string& fn, IDirect3DSurface9* surf);
--- source/imaging.cpp
+++ source/imaging.cpp
@@ -4,14 +4,6 @@
 #include "external/stb_image_write.h"
 #undef STB_IMAGE_WRITE_IMPLEMENTATION
 
-namespace {
-	inline void swap(BYTE& a, BYTE& b) {
-		BYTE tmp = b;
-		b = a;
-		a = tmp;
-	}
-}
-
 void ImageWriter::writeSurface(const string& fn, IDirect3DSurface9* surf) {
 	D3DSURFACE_DESC desc;
 	tempSurf->GetDesc(&desc);
@@ -20,7 +12,6 @@ void ImageWriter::writeSurface(const string& fn, IDirect3DSurface9* surf) {
 	r.top = 0;
 	r.right = desc.Width;
 	r.bottom = desc.Height;
-	if(future.valid()) future.get();
 	device9->StretchRect(surf, NULL, tempSurf, &r, D3DTEXF_NONE);
 	D3DLOCKED_RECT lockedR;
 	tempSurf->LockRect(&lockedR, &r, D3DLOCK_READONLY);
@@ -28,9 +19,9 @@ void ImageWriter::writeSurface(const string& fn, IDirect3DSurface9* surf) {
 	memcpy(buffer, lockedR.pBits, lockedR.Pitch*desc.Height);
 	INT pitch = lockedR.Pitch;
 	tempSurf->UnlockRect();
-	future = std::async([buffer, desc, pitch, fn] {
+	futures.push_back(std::async([buffer, desc, pitch, fn] {
 		for(unsigned i = 0; i < pitch*desc.Height; i += 4) {
-			// A8R8G8B8 --> RGBA
+			// A8R8G8B8 --> RGB
 			BYTE tmp = buffer[i + 0];
 			buffer[i + 0] = buffer[i + 2];
 			buffer[i + 1] = buffer[i + 1];
@@ -39,5 +30,5 @@ void ImageWriter::writeSurface(const string& fn, IDirect3DSurface9* surf) {
 		}
 		stbi_write_png(fn.c_str(), desc.Width, desc.Height, 4, buffer, pitch);
 		delete[] buffer;
-	});
+	}));
 }
