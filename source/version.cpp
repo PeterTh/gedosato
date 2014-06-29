71dc2057206f8eb05fc891a1416b02afed870a8a
Peter Thoman <peter@metaclassofnil.com>
2014-06-29 18:44:42 +0200

    Fixed unintentional build dependency on 'cp'. Thanks Alo!

--- source/version.cpp
+++ source/version.cpp
@@ -2,13 +2,13 @@
 
 #include "version.h"
 
-const char* VER_NAME = "Rapidity is a justice";
+const char* VER_NAME = "Underground Network";
 
 const unsigned VER_MAJOR = 0;
-const unsigned VER_MINOR = 8;
-const unsigned VER_BUILD = 825;
+const unsigned VER_MINOR = 9;
+const unsigned VER_BUILD = 826;
 
-const char* VER_STRING = "version 0.8.825 built on 2014/06/29 18:21:21";
+const char* VER_STRING = "version 0.9.826 built on 2014/06/29 18:43:36";
 
 #ifdef _DEBUG
 const char* MODE_STRING = "DEBUG";
