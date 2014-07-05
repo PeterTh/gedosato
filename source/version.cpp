3683e627056561863a04554ef965e8a10180fbec
Peter Thoman <peter@metaclassofnil.com>
2014-07-05 14:42:32 +0200

    Disable D3DPRESENTFLAG_DISCARD_DEPTHSTENCIL flag

--- source/version.cpp
+++ source/version.cpp
@@ -6,9 +6,9 @@ const char* VER_NAME = "Underground Network";
 
 const unsigned VER_MAJOR = 0;
 const unsigned VER_MINOR = 9;
-const unsigned VER_BUILD = 834;
+const unsigned VER_BUILD = 835;
 
-const char* VER_STRING = "version 0.9.834 built on 2014/07/03 19:32:51";
+const char* VER_STRING = "version 0.9.835 built on 2014/07/05 14:42:13";
 
 #ifdef _DEBUG
 const char* MODE_STRING = "DEBUG";
