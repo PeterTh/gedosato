9a3050c1cb53a5f0dec83c866dea467b016d82d9
Peter Thoman <peter@metaclassofnil.com>
2014-07-06 19:18:21 +0200

    Increased compatibility with additional injectors

--- source/version.cpp
+++ source/version.cpp
@@ -6,9 +6,9 @@ const char* VER_NAME = "Underground Network";
 
 const unsigned VER_MAJOR = 0;
 const unsigned VER_MINOR = 9;
-const unsigned VER_BUILD = 840;
+const unsigned VER_BUILD = 843;
 
-const char* VER_STRING = "version 0.9.840 built on 2014/07/05 15:12:27";
+const char* VER_STRING = "version 0.9.843 built on 2014/07/06 19:16:53";
 
 #ifdef _DEBUG
 const char* MODE_STRING = "DEBUG";
