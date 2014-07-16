312aa676cac91362c860912941f6c7508b5cf56d
Peter Thoman <peter@metaclassofnil.com>
2014-07-16 20:25:15 +0200

    Fixed regression in games which do not specify a backbuffer format

--- source/version.cpp
+++ source/version.cpp
@@ -6,9 +6,9 @@ const char* VER_NAME = "Poltergeist";
 
 const unsigned VER_MAJOR = 0;
 const unsigned VER_MINOR = 10;
-const unsigned VER_BUILD = 886;
+const unsigned VER_BUILD = 891;
 
-const char* VER_STRING = "version 0.10.886 built on 2014/07/16 19:30:25";
+const char* VER_STRING = "version 0.10.891 built on 2014/07/16 20:23:21";
 
 #ifdef _DEBUG
 const char* MODE_STRING = "DEBUG";
