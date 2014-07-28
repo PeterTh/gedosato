b3676b43565ef8fe47ec5b9b6aa90b4906117fcc
Peter Thoman <peter@metaclassofnil.com>
2014-07-28 19:26:15 +0200

    Hooking d3d debug and release dlls separately

--- source/version.cpp
+++ source/version.cpp
@@ -2,13 +2,13 @@
 
 #include "version.h"
 
-const char* VER_NAME = "Poltergeist";
+const char* VER_NAME = "At Sixes And Sevens";
 
 const unsigned VER_MAJOR = 0;
-const unsigned VER_MINOR = 10;
-const unsigned VER_BUILD = 927;
+const unsigned VER_MINOR = 11;
+const unsigned VER_BUILD = 929;
 
-const char* VER_STRING = "version 0.10.927 built on 2014/07/27 21:04:44";
+const char* VER_STRING = "version 0.11.929 built on 2014/07/28 19:25:22";
 
 #ifdef _DEBUG
 const char* MODE_STRING = "DEBUG";
