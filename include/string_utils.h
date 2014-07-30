afdee4a21d5da654138bf8174c58de3165d75907
Peter Thoman <peter@metaclassofnil.com>
2014-07-30 20:33:19 +0200

    Move format() to boost::format for type safety

--- include/string_utils.h
+++ include/string_utils.h
@@ -3,8 +3,26 @@
 #include <string>
 #include <regex>
 #include <boost/algorithm/string.hpp>
+#include <boost/format.hpp>
 
 using std::string;
 
-string format(const char* formatString, ...);
 bool matchWildcard(const string& str, const string& pattern);
+
+namespace {
+	using formatter = decltype(boost::format(""));
+
+	template<typename T>
+	formatter format_impl(formatter fmt, T elem) {
+		return fmt % elem;
+	}
+	template<typename T, typename... Rest>
+	formatter format_impl(formatter fmt, T elem, Rest... rest) {
+		return format_impl(fmt % elem, rest...);
+	}
+}
+
+template<typename...Rest> 
+string format(const char* formatString, Rest... rest) {
+	return format_impl(boost::format(formatString), rest...).str();
+}
--- source/string_utils.cpp
+++ source/string_utils.cpp
@@ -2,16 +2,6 @@
 
 #include <stdarg.h>
 
-string format(const char* formatString, ...) {
-	const unsigned BUFFER_SIZE = 2048 * 8;
-	char buffer[BUFFER_SIZE];
-	va_list arglist;
-	va_start(arglist, formatString);
-	vsnprintf_s(buffer, BUFFER_SIZE, formatString, arglist);
-	va_end(arglist);
-	return string(buffer);
-}
-
 bool matchWildcard(const string& str, const string& pattern) {
 	string regex = pattern;
 	const char* escapeChars[] = { ".", "+", "(", ")", "[", "]", "^", "$"};
