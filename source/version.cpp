ed70e58459bc825a2738d68010458a0834c0833a
Peter Thoman <peter@metaclassofnil.com>
2014-08-06 22:13:44 +0200

    Made presentInterval setting work when not downsampling

--- source/version.cpp
+++ source/version.cpp
@@ -6,9 +6,9 @@ const char* VER_NAME = "At Sixes And Sevens";
 
 const unsigned VER_MAJOR = 0;
 const unsigned VER_MINOR = 11;
-const unsigned VER_BUILD = 982;
+const unsigned VER_BUILD = 985;
 
-const char* VER_STRING = "version 0.11.982 built on 2014/08/05 21:40:04";
+const char* VER_STRING = "version 0.11.985 built on 2014/08/06 22:06:30";
 
 #ifdef _DEBUG
 const char* MODE_STRING = "DEBUG";
