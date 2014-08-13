646de3eed18852272fbbea169f6d75518f12bc89
Peter Thoman <peter@metaclassofnil.com>
2014-08-13 20:37:55 +0200

    Automatic Updating

--- source/version.cpp
+++ source/version.cpp
@@ -6,9 +6,9 @@ const char* VER_NAME = "At Sixes And Sevens";
 
 const unsigned VER_MAJOR = 0;
 const unsigned VER_MINOR = 11;
-const unsigned VER_BUILD = 1163;
+const unsigned VER_BUILD = 1164;
 
-const char* VER_STRING = "version 0.11.1163 built on 2014/08/12 21:50:54";
+const char* VER_STRING = "version 0.11.1164 built on 2014/08/13 20:36:25";
 
 #ifdef _DEBUG
 const char* MODE_STRING = "DEBUG";
