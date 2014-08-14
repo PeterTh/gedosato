ef6e9f85d5422d4bcec488db09e732bc3993383b
Peter Thoman <peter@metaclassofnil.com>
2014-08-14 21:43:40 +0200

    Gracefully handle errors when checking for updates

--- source/version.cpp
+++ source/version.cpp
@@ -6,9 +6,9 @@ const char* VER_NAME = "Sepia Recollections";
 
 const unsigned VER_MAJOR = 0;
 const unsigned VER_MINOR = 12;
-const unsigned VER_BUILD = 1168;
+const unsigned VER_BUILD = 1169;
 
-const char* VER_STRING = "version 0.12.1168 built on 2014/08/14 21:33:25";
+const char* VER_STRING = "version 0.12.1169 built on 2014/08/14 21:42:47";
 
 #ifdef _DEBUG
 const char* MODE_STRING = "DEBUG";
