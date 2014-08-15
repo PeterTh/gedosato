7447f2551a4320fe1151e0c03ffed2d93bfdc62b
Peter Thoman <peter@metaclassofnil.com>
2014-08-15 12:36:46 +0200

    PresentWidth/Height now defaults to the primary monitor resolution

--- source/version.cpp
+++ source/version.cpp
@@ -6,9 +6,9 @@ const char* VER_NAME = "Sepia Recollections";
 
 const unsigned VER_MAJOR = 0;
 const unsigned VER_MINOR = 12;
-const unsigned VER_BUILD = 1184;
+const unsigned VER_BUILD = 1185;
 
-const char* VER_STRING = "version 0.12.1184 built on 2014/08/15 12:05:11";
+const char* VER_STRING = "version 0.12.1185 built on 2014/08/15 12:22:32";
 
 #ifdef _DEBUG
 const char* MODE_STRING = "DEBUG";
