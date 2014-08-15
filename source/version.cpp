c6105edb24dd6324844400df1a0f12523d458ebc
Peter Thoman <peter@metaclassofnil.com>
2014-08-15 22:47:55 +0200

    Added SCREEN_SIZE macro definition for postprocessing shaders

--- source/version.cpp
+++ source/version.cpp
@@ -6,9 +6,9 @@ const char* VER_NAME = "Sepia Recollections";
 
 const unsigned VER_MAJOR = 0;
 const unsigned VER_MINOR = 12;
-const unsigned VER_BUILD = 1192;
+const unsigned VER_BUILD = 1194;
 
-const char* VER_STRING = "version 0.12.1192 built on 2014/08/15 14:43:00";
+const char* VER_STRING = "version 0.12.1194 built on 2014/08/15 22:40:24";
 
 #ifdef _DEBUG
 const char* MODE_STRING = "DEBUG";
