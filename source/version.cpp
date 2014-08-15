14b583ba6ca78db10fe2a6b1e396bbeade843629
Peter Thoman <peter@metaclassofnil.com>
2014-08-15 11:37:59 +0200

    More state fixes

--- source/version.cpp
+++ source/version.cpp
@@ -6,9 +6,9 @@ const char* VER_NAME = "Sepia Recollections";
 
 const unsigned VER_MAJOR = 0;
 const unsigned VER_MINOR = 12;
-const unsigned VER_BUILD = 1169;
+const unsigned VER_BUILD = 1182;
 
-const char* VER_STRING = "version 0.12.1169 built on 2014/08/14 21:42:47";
+const char* VER_STRING = "version 0.12.1182 built on 2014/08/15 11:32:47";
 
 #ifdef _DEBUG
 const char* MODE_STRING = "DEBUG";
