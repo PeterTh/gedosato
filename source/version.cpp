367355c7c2d435cb2088c96773753090e66f5fa2
Peter Thoman <peter@metaclassofnil.com>
2014-09-13 19:03:22 +0200

    Fixed gedosatotool postprocessing editor

--- source/version.cpp
+++ source/version.cpp
@@ -6,9 +6,9 @@ const char* VER_NAME = "Cross Again";
 
 const unsigned VER_MAJOR = 0;
 const unsigned VER_MINOR = 13;
-const unsigned VER_BUILD = 1442;
+const unsigned VER_BUILD = 1443;
 
-const char* VER_STRING = "version 0.13.1442 built on 2014/09/11 15:25:38";
+const char* VER_STRING = "version 0.13.1443 built on 2014/09/13 19:02:57";
 
 #ifdef _DEBUG
 const char* MODE_STRING = "DEBUG";
