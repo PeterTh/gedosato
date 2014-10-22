dd679f8d048180d1a0bc60aa2a93ccc4f541c198
Peter Thoman <peter@metaclassofnil.com>
2014-10-22 20:19:46 +0200

    Fix mouse cursor in windowed downsampling

--- source/detouring.cpp
+++ source/detouring.cpp
@@ -403,8 +403,11 @@ GENERATE_INTERCEPT_HEADER(ClipCursor, BOOL, WINAPI, __in_opt CONST RECT *lpRect)
 }
 
 GENERATE_INTERCEPT_HEADER(WindowFromPoint, HWND, WINAPI, _In_ POINT Point) {
-	return GetActiveWindow();
-	//return TrueWindowFromPoint(Point);
+	if(RSManager::currentlyDownsampling() && Settings::get().getModifyGetCursorPos()) {
+		Point.x = Point.x * Settings::get().getPresentWidth() / Settings::get().getRenderWidth();
+		Point.y = Point.y * Settings::get().getPresentHeight() / Settings::get().getRenderHeight();
+	}
+	return TrueWindowFromPoint(Point);
 }
 
 // Messages /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
