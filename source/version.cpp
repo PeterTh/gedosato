be071d9976cb5fb68b7e4c7457f236aa714a806d
Peter Thoman <peter@metaclassofnil.com>
2014-10-22 20:09:23 +0200

    Version bump

--- source/version.cpp
+++ source/version.cpp
@@ -2,13 +2,13 @@
 
 #include "version.h"
 
-const char* VER_NAME = "Barrage am Ring";
+const char* VER_NAME = "Tokikagura";
 
 const unsigned VER_MAJOR = 0;
-const unsigned VER_MINOR = 14;
-const unsigned VER_BUILD = 1620;
+const unsigned VER_MINOR = 15;
+const unsigned VER_BUILD = 1621;
 
-const char* VER_STRING = "version 0.14.1620 built on 2014/10/22 19:15:19";
+const char* VER_STRING = "version 0.15.1621 built on 2014/10/22 20:09:01";
 
 #ifdef _DEBUG
 const char* MODE_STRING = "DEBUG";
