8a72c079ea1dfe259665019d647e8b0c30e64587
Peter Thoman <peter@metaclassofnil.com>
2014-11-30 18:50:18 +0100

    Version 0.16

--- source/version.cpp
+++ source/version.cpp
@@ -2,13 +2,13 @@
 
 #include "version.h"
 
-const char* VER_NAME = "Tokikagura";
+const char* VER_NAME = "Third Ensemble";
 
 const unsigned VER_MAJOR = 0;
-const unsigned VER_MINOR = 15;
-const unsigned VER_BUILD = 1684;
+const unsigned VER_MINOR = 16;
+const unsigned VER_BUILD = 1704;
 
-const char* VER_STRING = "version 0.15.1684 built on 2014/11/09 19:35:31";
+const char* VER_STRING = "version 0.16.1704 built on 2014/11/30 18:46:26";
 
 #ifdef _DEBUG
 const char* MODE_STRING = "DEBUG";
