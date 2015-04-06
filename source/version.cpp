a412fc6802339c0c1ded34812dc79fe0c3473d1e
Peter Thoman <peter@metaclassofnil.com>
2015-04-06 19:00:34 +0200

    Version bump to 0.17

--- source/version.cpp
+++ source/version.cpp
@@ -2,13 +2,13 @@
 
 #include "version.h"
 
-const char* VER_NAME = "Third Ensemble";
+const char* VER_NAME = "Forlane | Rei";
 
 const unsigned VER_MAJOR = 0;
-const unsigned VER_MINOR = 16;
-const unsigned VER_BUILD = 1770;
+const unsigned VER_MINOR = 17;
+const unsigned VER_BUILD = 1771;
 
-const char* VER_STRING = "version 0.16.1770 built on 2014/12/25 18:43:44";
+const char* VER_STRING = "version 0.17.1771 built on 2015/04/06 17:36:14";
 
 #ifdef _DEBUG
 const char* MODE_STRING = "DEBUG";
