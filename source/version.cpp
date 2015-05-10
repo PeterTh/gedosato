dbc13d4435b95e9b47b04267e84238d0cf84abab
Peter Thoman <peter@metaclassofnil.com>
2015-05-10 21:50:23 +0200

    GeDoSaToTool cast bug fix

--- source/version.cpp
+++ source/version.cpp
@@ -6,9 +6,9 @@ const char* VER_NAME = "Forlane | Rei";
 
 const unsigned VER_MAJOR = 0;
 const unsigned VER_MINOR = 17;
-const unsigned VER_BUILD = 1791;
+const unsigned VER_BUILD = 1792;
 
-const char* VER_STRING = "version 0.17.1791 built on 2015/05/09 23:55:35";
+const char* VER_STRING = "version 0.17.1792 built on 2015/05/10 21:49:31";
 
 #ifdef _DEBUG
 const char* MODE_STRING = "DEBUG";
