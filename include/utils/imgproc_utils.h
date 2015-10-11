2f75793703dfd4225937b95b67f3ca7a7b087bc3
PeterTh <peter@metaclassofnil.com>
2015-10-11 19:57:20 +0200

    Add threading and image processing utilities

---
+++ include/utils/imgproc_utils.h
@@ -0,0 +1,80 @@
+#pragma once
+
+#define u8 uint8_t
+#define u16 uint16_t
+#define u32 uint32_t
+
+#include <vector>
+
+template <typename T>
+class SimpleBuf {
+public:
+	SimpleBuf() : buf_(0), size_(0) {}
+
+	SimpleBuf(size_t size) : buf_(0) {
+		resize(size);
+	}
+
+	~SimpleBuf() {
+		if(buf_ != 0) {
+			free(buf_);
+		}
+	}
+
+	inline T &operator[](size_t index) {
+		return buf_[index];
+	}
+
+	// Doesn't preserve contents.
+	void resize(size_t size) {
+		if(size_ < size) {
+			if(buf_ != 0) {
+				free(buf_);
+			}
+			buf_ = (T*)malloc(size * sizeof(T));
+			size_ = size;
+		}
+	}
+
+	T *data() {
+		return buf_;
+	}
+
+	size_t size() const {
+		return size_;
+	}
+
+private:
+	T *buf_;
+	size_t size_;
+};
+
+class TextureScaler {
+public:
+	TextureScaler();
+	~TextureScaler();
+
+	void Scale(u32* &data, u32 &dstfmt, int &width, int &height, int factor);
+
+	enum { XBRZ = 0, HYBRID = 1, BICUBIC = 2, HYBRID_BICUBIC = 3 };
+
+private:
+	virtual void ConvertTo8888(u32 format, u32* source, u32* &dest, int width, int height) = 0;
+	virtual int BytesPerPixel(u32 format) = 0;
+	virtual u32 Get8888Format() = 0;
+
+	void ScaleXBRZ(int factor, u32* source, u32* dest, int width, int height);
+	void ScaleBilinear(int factor, u32* source, u32* dest, int width, int height);
+	void ScaleBicubicBSpline(int factor, u32* source, u32* dest, int width, int height);
+	void ScaleBicubicMitchell(int factor, u32* source, u32* dest, int width, int height);
+	void ScaleHybrid(int factor, u32* source, u32* dest, int width, int height, bool bicubic = false);
+
+	void DePosterize(u32* source, u32* dest, int width, int height);
+
+	bool IsEmptyOrFlat(u32* data, int pixels, int fmt);
+
+	// depending on the factor and texture sizes, these can get pretty large 
+	// maximum is (100 MB total for a 512 by 512 texture with scaling factor 5 and hybrid scaling)
+	// of course, scaling factor 5 is totally silly anyway
+	SimpleBuf<u32> bufInput, bufDeposter, bufOutput, bufTmp1, bufTmp2, bufTmp3;
+};
---
+++ source/utils/thread_utils.cpp
@@ -0,0 +1,144 @@
+
+#include "utils/thread_utils.h"
+
+#include "settings.h"
+
+///////////////////////////// WorkerThread
+
+WorkerThread::WorkerThread() : active(true), started(false) {
+	thread = new std::thread(std::bind(&WorkerThread::WorkFunc, this));
+	doneMutex.lock();
+	while(!started) {};
+}
+
+WorkerThread::~WorkerThread() {
+	mutex.lock();
+	active = false;
+	signal.notify_one();
+	mutex.unlock();
+	thread->join();
+	delete thread;
+}
+
+void WorkerThread::Process(const std::function<void()>& work) {
+	mutex.lock();
+	work_ = work;
+	signal.notify_one();
+	mutex.unlock();
+}
+
+void WorkerThread::WaitForCompletion() {
+	std::unique_lock<std::recursive_mutex> lock(doneMutex);
+	done.wait(lock);
+}
+
+void WorkerThread::WorkFunc() {
+	mutex.lock();
+	std::unique_lock<std::recursive_mutex> lock(mutex);
+	started = true;
+	while(active) {
+		signal.wait(lock);
+		if(active) {
+			work_();
+			doneMutex.lock();
+			done.notify_one();
+			doneMutex.unlock();
+		}
+	}
+}
+
+LoopWorkerThread::LoopWorkerThread() : WorkerThread(true) {
+	thread = new std::thread(std::bind(&LoopWorkerThread::WorkFunc, this));
+	doneMutex.lock();
+	while(!started) {};
+}
+
+void LoopWorkerThread::Process(const std::function<void(int, int)> &work, int start, int end) {
+	mutex.lock();
+	work_ = work;
+	start_ = start;
+	end_ = end;
+	signal.notify_one();
+	mutex.unlock();
+}
+
+void LoopWorkerThread::WorkFunc() {
+	mutex.lock();
+	started = true;
+	while(active) {
+		signal.wait(mutex);
+		if(active) {
+			work_(start_, end_);
+			doneMutex.lock();
+			done.notify_one();
+			doneMutex.unlock();
+		}
+	}
+}
+
+///////////////////////////// ThreadPool
+
+ThreadPool::ThreadPool(int numThreads) : workersStarted(false) {
+	if(numThreads <= 0) {
+		numThreads_ = 1;
+		SDLOG(-1, "ThreadPool: Bad number of threads %i", numThreads);
+	}
+	else if(numThreads > 8) {
+		SDLOG(-1, "ThreadPool: Capping number of threads to 8 (was %i)", numThreads);
+		numThreads_ = 8;
+	}
+	else {
+		numThreads_ = numThreads;
+	}
+}
+
+void ThreadPool::StartWorkers() {
+	if(!workersStarted) {
+		for(int i = 0; i < numThreads_; ++i) {
+			workers.push_back(std::make_shared<LoopWorkerThread>());
+		}
+		workersStarted = true;
+	}
+}
+
+void ThreadPool::ParallelLoop(const std::function<void(int, int)> &loop, int lower, int upper) {
+	int range = upper - lower;
+	if(range >= numThreads_ * 2) { // don't parallelize tiny loops (this could be better, maybe add optional parameter that estimates work per iteration)
+		std::lock_guard<std::recursive_mutex> guard(mutex);
+		StartWorkers();
+
+		// could do slightly better load balancing for the generic case, 
+		// but doesn't matter since all our loops are power of 2
+		int chunk = range / numThreads_;
+		int s = lower;
+		for(int i = 0; i < numThreads_ - 1; ++i) {
+			workers[i]->Process(loop, s, s + chunk);
+			s += chunk;
+		}
+		// This is the final chunk.
+		loop(s, upper);
+		for(int i = 0; i < numThreads_ - 1; ++i) {
+			workers[i]->WaitForCompletion();
+		}
+	}
+	else {
+		loop(lower, upper);
+	}
+}
+
+///////////////////////////// GlobalThreadPool
+
+std::shared_ptr<ThreadPool> GlobalThreadPool::pool;
+bool  GlobalThreadPool::initialized = false;
+
+void GlobalThreadPool::Loop(const std::function<void(int, int)>& loop, int lower, int upper) {
+	Inititialize();
+	pool->ParallelLoop(loop, lower, upper);
+}
+
+void GlobalThreadPool::Inititialize() {
+	if(!initialized) {
+		pool = std::make_shared<ThreadPool>(std::thread::hardware_concurrency());
+		initialized = true;
+	}
+}
