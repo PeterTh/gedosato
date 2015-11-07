7366e8b0c79f32c1feca9970b5c7f4dbff4fba0b
PeterTh <peter@metaclassofnil.com>
2015-11-07 18:02:50 +0100

    Fix version mismatch

--- source/version.cpp
+++ source/version.cpp
@@ -6,9 +6,9 @@ const char* VER_NAME = "Chaos Flare";
 
 const unsigned VER_MAJOR = 0;
 const unsigned VER_MINOR = 19;
-const unsigned VER_BUILD = 1985;
+const unsigned VER_BUILD = 1986;
 
-const char* VER_STRING = "version 0.19.1985 built on 2015/11/07 17:10:29";
+const char* VER_STRING = "version 0.19.1986 built on 2015/11/07 18:02:22";
 
 #ifdef _DEBUG
 const char* MODE_STRING = "DEBUG";
