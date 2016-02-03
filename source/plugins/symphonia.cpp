6bfc3ab5ec1470be96a793f6ee37fc05fdcafe75
PeterTh <peter@metaclassofnil.com>
2016-02-03 16:08:43 +0100

    Fix ToS postprocessing

--- source/plugins/symphonia.cpp
+++ source/plugins/symphonia.cpp
@@ -20,25 +20,43 @@ void SymphoniaPlugin::prePresent() {
 
 HRESULT SymphoniaPlugin::redirectCreateTexture(UINT Width, UINT Height, UINT Levels, DWORD Usage, D3DFORMAT Format, D3DPOOL Pool, IDirect3DTexture9** ppTexture, HANDLE* pSharedHandle) {
 	if(Width == 1280 && Height == 720 && (((Usage & D3DUSAGE_RENDERTARGET) && Format == D3DFMT_A8R8G8B8) || Format == D3DFMT_D24S8)) {
-		SDLOG(-1, "CreateTexture override\n");
+		SDLOG(-1, "CreateTexture BB override\n");
 		Width = Settings::get().getRenderWidth();
 		Height = Settings::get().getRenderHeight();
+	}
+	else if(Width == 512 && Height == 256 && (Usage & D3DUSAGE_RENDERTARGET)) {
+		SDLOG(-1, "CreateTexture effect override\n");
+		Width = Settings::get().getRenderWidth() / 2;
+		Height = Settings::get().getRenderHeight() / 2;
 	}
 	return GenericPlugin::redirectCreateTexture(Width, Height, Levels, Usage, Format, Pool, ppTexture, pSharedHandle);
 }
 
 HRESULT SymphoniaPlugin::redirectSetViewport(CONST D3DVIEWPORT9* pViewport) {
 	if(pViewport->Width == 1280 && pViewport->Height == 720) {
-		SDLOG(4, "Symphonia: SetViewport override");
+		SDLOG(4, "Symphonia: SetViewport BB override");
 		D3DVIEWPORT9 newView = *pViewport;
 		newView.Width = Settings::get().getRenderWidth();
 		newView.Height = Settings::get().getRenderHeight();
+		return GenericPlugin::redirectSetViewport(&newView);
+	} 
+	else if(pViewport->Width == 512 && pViewport->Height == 256) {
+		SDLOG(4, "Symphonia: SetViewport effect override");
+		D3DVIEWPORT9 newView = *pViewport;
+		newView.Width = Settings::get().getRenderWidth() / 2;
+		newView.Height = Settings::get().getRenderHeight() / 2;
 		return GenericPlugin::redirectSetViewport(&newView);
 	}
 	return GenericPlugin::redirectSetViewport(pViewport);
 }
 
 HRESULT SymphoniaPlugin::redirectSetVertexShaderConstantF(UINT StartRegister, CONST float* pConstantData, UINT Vector4fCount) {
+	if(StartRegister == 240 && Vector4fCount == 1) {
+		D3DVIEWPORT9 vp;
+		d3ddev->GetViewport(&vp);
+		float vals[4] = { -1.0f/vp.Width, 1.0f/vp.Height, 0.0f, 0.0f };
+		return GenericPlugin::redirectSetVertexShaderConstantF(StartRegister, vals, Vector4fCount);
+	}
 	return GenericPlugin::redirectSetVertexShaderConstantF(StartRegister, pConstantData, Vector4fCount);
 }
 
--- source/version.cpp
+++ source/version.cpp
@@ -6,9 +6,9 @@ const char* VER_NAME = "How Could Hell Be Any Worse";
 
 const unsigned VER_MAJOR = 0;
 const unsigned VER_MINOR = 21;
-const unsigned VER_BUILD = 2128;
+const unsigned VER_BUILD = 2132;
 
-const char* VER_STRING = "version 0.21.2128 built on 2016/02/02 22:21:42";
+const char* VER_STRING = "version 0.21.2132 built on 2016/02/03 09:46:43";
 
 #ifdef _DEBUG
 const char* MODE_STRING = "DEBUG";
